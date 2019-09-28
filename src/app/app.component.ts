31c0f73b6a1631c1151c4cd24e8478d99418d87b
Bence Ludmány <mokkants@gmail.com>
2019-09-28 18:02:34 +0200

    Create camera feed on phones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'justify';
+
+  @ViewChild("video",{static:true}) video : ElementRef;
+
+    public constructor() {
+      this.handleVideo = this.handleVideo.bind(this);
+    }
+
+    public async ngAfterViewInit() {
+        if('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+          const stream = await navigator.mediaDevices.getUserMedia({ video: {facingMode:{ exact: "environment" }}});
+          this.handleVideo(stream);
+        }
+    }
+
+    handleVideo(stream){
+      this.video.nativeElement.srcObject = stream;
+    }
 }
