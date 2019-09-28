43c6c2b6a9690ee5936d1a664bb98b81d5aa0dbe
Bence Ludmány <mokkants@gmail.com>
2019-09-29 01:27:18 +0200

    Add loading screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingScreenComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/loading-screen/loading-screen.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-screen',
+  templateUrl: './loading-screen.component.html',
+  styleUrls: ['./loading-screen.component.css']
+})
+export class LoadingScreenComponent implements OnInit {
+
+  loading = true;
+  logo = "/assets/logo.png";
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
