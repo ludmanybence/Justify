93059bef38f9dc7bcf4d31f78dd06788cd911960
Bence Ludmány <mokkants@gmail.com>
2019-09-29 05:02:44 +0200

    Add jquery to project for FFrostyy's ajax call

--- src/app/services/ocr.service.ts
+++ src/app/services/ocr.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { TesseractWorker } from 'tesseract.js';
+
+import * as $ from 'jquery';
 
 @Injectable({
   providedIn: 'root'
@@ -10,28 +11,47 @@ export class OcrService {
     this.processImage = this.processImage.bind(this);
   }
 
-  _res : any;
-  
-
-  get res(){
-    return this._res;
-  }
-  
-  set res(res){
-    this._res = res;
-  }
-
   processImage(sourceImageUrl) {
-  const worker = new TesseractWorker();
+        let subscriptionKey = "c7fc33e4aaee4ea1863227ff796f6ba8";
+        let endpoint = "https://northeurope.api.cognitive.microsoft.com/";
+        if (!subscriptionKey) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
+        
+        var uriBase = endpoint + "vision/v2.0/ocr";
+        // Request parameters.
+        var params = {
+            "language": "unk",
+            "detectOrientation": "true",
+        };
+        // Perform the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+            // Request headers.
+            beforeSend: function(jqXHR){
+                jqXHR.setRequestHeader("Content-Type","application/json");
+                jqXHR.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+            type: "POST",
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+        .done(function(data) {
+            
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ?
+                "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+                (jQuery.parseJSON(jqXHR.responseText).message) ?
+                    jQuery.parseJSON(jqXHR.responseText).message :
+                    jQuery.parseJSON(jqXHR.responseText).error.message;
+            alert(errorString);
+        });
+    };
 
-  worker.recognize(sourceImageUrl, 'eng')
-  .progress(progress => {
-    console.log('progress', progress);
-  }).then(result => {
-    console.log('result', result);
-    this.res = result;
-  });
-
+    findProduct(data)
+    {
+        
+    };
 
   }
-}
